afef450250ae4a35d118fdca09c049f21490c188
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-08 19:49:24 +0200

    complete variable list design

--- src/app/variable-list/variable-list.component.ts
+++ src/app/variable-list/variable-list.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-variable-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './variable-list.component.html',
-  styleUrl: './variable-list.component.scss'
+  styleUrl: './variable-list.component.scss',
 })
 export class VariableListComponent {
-
+  processName: string = 'ProcessName';
 }
