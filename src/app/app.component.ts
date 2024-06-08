7fee9873d0d75b21c74a21d437d05245d5a25f6f
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-08 16:45:49 +0200

    add all components to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CredentialsComponent } from './credentials/credentials.component';
+import { CallButtonsComponent } from './call-buttons/call-buttons.component';
+import { ParentStepListComponent } from './parent-step-list/parent-step-list.component';
+import { VariableListComponent } from './variable-list/variable-list.component';
+import { VariableOccurenceTableComponent } from './variable-occurence-table/variable-occurence-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatGridListModule,
+    CredentialsComponent,
+    CallButtonsComponent,
+    ParentStepListComponent,
+    VariableListComponent,
+    VariableOccurenceTableComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
