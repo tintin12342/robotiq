a4838fd404f6e48a1b5d1749068ec04dda465294
Tin Vaiti <tin.vaiti5@gmail.com>
2024-06-08 20:05:06 +0200

    complete parent step list design

--- src/app/parent-step-list/parent-step-list.component.ts
+++ src/app/parent-step-list/parent-step-list.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-parent-step-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './parent-step-list.component.html',
   styleUrl: './parent-step-list.component.scss'
 })
